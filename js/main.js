da57359fcff2b832e7819d6ebcf5cc52b3174cb0
ebmcambodia <ebmcambodia069@gmail.com>
2025-02-13 16:18:57 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,3 +79,25 @@
     
 })(jQuery);
 
+
+$(document).ready(function(){
+    $('.logo-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+});
